1f57f202ba039cd79b9d2570e0fbe8fb10b9d407
davidbaschung <davidbaschung@bluewin.ch>
2022-08-11 16:11:54 +0200

    feats: tree-leafs, colors, transitions. Fix: corrections, comments.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/*  #region utils */
+/*     #region utils     */
 
 function $(id) {
     return document.getElementById(id);
@@ -19,112 +19,29 @@ function log(tag='', ...contents) {
     }
 }
 
-/* #endregion */
+/*     #endregion     */
 
 
 
-/*  #region runtime variables */
 
-// let initialNbPersonsInput = 3;
+
+/*     #region global variables and parameters     */
+
 let personsData = [];   /* JSON data for every person */
 let randomTree = null;  /* random Tree composed with structural PersonTreeNodes */
-let initialNbPersonsInput = 100;
-// let domTree;            /* the HTML element with the "tree" ID */
+let initialNbPersonsInput = 1000; /* The initial 
 
 /*  onPageLoaded is called later */
 
-/* #endregion */
+/*     #endregion     */
 
 
 
-/* #region SPA methods & definitions */
 
-customElements.define("person-display",
-    class extends HTMLElement {
-        constructor() {
-            super();
-            const template = $("person-display-template").content;
-            const shadowRoot = this.attachShadow({mode: 'open'});
-            let newNode = template.cloneNode(true);
-            shadowRoot.appendChild(newNode);
-        }
-    }
-);
 
-//TODO remove
-document.addEventListener("togglechildren", (customEvent) => {
-    log("customevent", customEvent)
-    log("id found",customEvent.detail.personID);
-    log("target", customEvent.target);
-    // recDisplayPersons
-});
+/*     #region Tree Model     */
 
-document.addEventListener("DOMContentLoaded", () => {
-    let lazyBackgroundElement = document.querySelector(".lazy-background");
-    if ("IntersectionObserver" in window) {
-        let lazyObserver = new IntersectionObserver( (entries, observer) => {
-            entries.forEach( (entry) => {
-                if (entry.isIntersecting) {
-                    // unloadPreloadedElements();
-                }
-            });
-        });
-        lazyObserver.observe(lazyBackgroundElement);
-    } else log("no intersection observer");
-});
-
-function onPageLoaded() {
-    unloadPreloadedElements();
-//     // domTree = $('tree');
-    if (initialNbPersonsInput != undefined) {
-        const input = $('nbPersonsInput');
-        input.value=initialNbPersonsInput;
-        input.dispatchEvent(new KeyboardEvent('keydown', {keyCode:13}));
-    }
-//     // createPersons(initialNbPersonsInput);
-}
-
-function unloadPreloadedElements() {
-    $('loading-image').remove();
-    let lazyBackgroundElement = document.querySelector(".lazy-background");
-    lazyBackgroundElement.classList.remove("lazy-background");
-    lazyBackgroundElement.classList.add("background");
-}
-
-/* generates a random full tree from a JSON array */
-function generatePersonsRandomTree(jsonArray) {
-    let t0 = performance.now();
-    class PersonTreeNode {
-        constructor(parent=null, index=0) {
-            this.parentNode = parent;
-            this.childNodes = [];
-            this.jsonIndex = index; /* data index and also DOM element ID */
-            // this.domElementNode = ... // TODO
-        }
-    }
-    let jsonLowIndex = 0;
-    let tree = new PersonTreeNode();
-    jsonLowIndex++;
-    let nodesBreadthQueue = [tree];
-    while ( jsonLowIndex < jsonArray.length ) {
-        let currentPerson = nodesBreadthQueue.shift();
-        const remainingPersons = jsonArray.length-jsonLowIndex;
-        if (remainingPersons==0)
-            return;
-        const randomNbchildren = Math.ceil(Math.random() * 5); /* breadth-first generating, up to 5 children */
-        const nbchildren = Math.min(randomNbchildren, remainingPersons);
-        for (let i=nbchildren; i>0; i--) {
-            let child = new PersonTreeNode(currentPerson, jsonLowIndex++);
-            // log("new child", jsonLowIndex, child, currentPerson);
-            currentPerson.childNodes.push(child);
-            nodesBreadthQueue.push(child);
-        }
-    }
-    let t1 = performance.now() - t0;
-    log(`generatePersonsRandomTree performance (ms) with ${jsonArray.length} users`, t1)
-    return tree;
-}
-
+/* Launched from DOM input elements. Starts the persons generating process */
 async function createPersons(amount) {
     if (
         parseInt(amount) > 5000
@@ -138,67 +55,12 @@ async function createPersons(amount) {
     // initialNbPersonsInput = amount;
     await loadPersons(amount);
     randomTree = generatePersonsRandomTree(personsData);
-    const treeContainer = $("person0");
+    const treeContainer = $("treeContainer");
     treeContainer.innerHTML = "";
     treeContainer.appendChild(applyPersonTemplate(randomTree));
 }
 
-function developChildren(personTreeNode, mustDisplay) {
-    const personContainer = $("person"+personTreeNode.jsonIndex);
-    if (mustDisplay) {
-        const newList = document.createElement("ul");
-        personContainer.appendChild(newList);
-        recDevelopChild(personTreeNode, [...personTreeNode.childNodes]);
-    } else {
-        const ul = document.querySelector(`#person${personTreeNode.jsonIndex} ul:first-of-type`);
-        if (ul)
-            ul.remove();
-    }
-}
-
-function recDevelopChild(personTreeNode, children) {
-    if (children.length==0)
-        return;
-    const personContainer = document.querySelector(`#person${personTreeNode.jsonIndex}`);
-    const childNode = children.shift();
-    const childElement = applyPersonTemplate(childNode);
-    const listElement = document.createElement("li");
-    const unorderedList = personContainer.querySelector(`ul:first-of-type`);
-    unorderedList.appendChild(childElement);
-    personContainer.appendChild(listElement);
-    recDevelopChild(personTreeNode, children);
-    // const firstPerson = persons[0];
-    // for (key in firstPerson) {
-    //     domTree.innerHTML += `<li>${key}</li>`;
-    //     if (typeof firstPerson[key] == 'string');
-    // }
-
-    // TODO show DOM tree branches from random tree
-}
-
-function applyPersonTemplate(personTreeNode) {
-    // const childrenToDisplay = personTreeNode.childNodes;
-    // for (let cNode of childrenToDisplay) {
-    const cNode = personTreeNode; //TODO check
-    const cData = personsData[cNode.jsonIndex];
-    let personContainer = document.createElement("div");
-    personContainer.id = "person"+cNode.jsonIndex;
-    personContainer.isOpened = "false";
-    let personDisplay = document.createElement("person-display");
-    personDisplay.innerHTML = `
-        <img slot="portrait" src="${cData.picture.large}" class="img" alt="portrait not found" />
-        <span slot="first">${cData.name.first}</span>
-        <span slot="last">${cData.name.last}</span>`;
-    personDisplay.onclick = (event) => {
-        const isOpened = JSON.parse(personContainer.isOpened);
-        personContainer.isOpened = JSON.stringify(! isOpened);
-        developChildren(personTreeNode, ! isOpened );
-    }
-    personContainer.appendChild(personDisplay);
-    return personContainer;
-    // }
-}
-
+/* Loads the JSON data from the server for all listed persons, without images */
 async function loadPersons(amount) {
     $("loading").removeAttribute("hidden");
     async function fetchFile() {
@@ -222,7 +84,7 @@ async function loadPersons(amount) {
             personsData = json.results;
         }).catch(async () => {
             log("JSON file request : failed")
-            await new Promise((resolve) => setTimeout(resolve, 1000));
+            await new Promise((resolve) => setTimeout(resolve, 10000));
             fetchFile();
         });
     }
@@ -230,9 +92,165 @@ async function loadPersons(amount) {
     $("loading").setAttribute("hidden",'');
 }
 
+/* Generates a random full tree of persons from a JSON array */
+function generatePersonsRandomTree(jsonArray) {
+    let t0 = performance.now();
+    class PersonTreeNode {
+        constructor(parent=null, index=0) {
+            this.parentNode = parent;
+            this.childNodes = [];
+            this.jsonIndex = index; /* data index and also DOM element ID */
+        }
+    }
+    let jsonLowIndex = 0;
+    let tree = new PersonTreeNode();
+    jsonLowIndex++;
+    let nodesBreadthQueue = [tree];
+    while ( jsonLowIndex < jsonArray.length ) {
+        let currentPerson = nodesBreadthQueue.shift();
+        const remainingPersons = jsonArray.length-jsonLowIndex;
+        if (remainingPersons==0)
+            return;
+        const randomNbchildren = Math.ceil(Math.random() * 5); /* breadth-first generating, up to 5 children */
+        const nbchildren = Math.min(randomNbchildren, remainingPersons);
+        for (let i=nbchildren; i>0; i--) {
+            let child = new PersonTreeNode(currentPerson, jsonLowIndex++);
+            currentPerson.childNodes.push(child);
+            nodesBreadthQueue.push(child);
+        }
+    }
+    let t1 = performance.now() - t0;
+    log(`generatePersonsRandomTree performance (ms) with ${jsonArray.length} users`, t1)
+    return tree;
+}
+
+/*     endregion     */
+
+
+
+
+
+/*     #region Persons Views     */
+
+/* listens the inputed keyboard events */
 function nbPersonsInput($event, amount) {
     if ($event.keyCode==13)
         createPersons(amount);
 }
 
-/* #endregion */
+/* defines the person's view template */
+customElements.define("person-display",
+    class extends HTMLElement {
+        constructor(is) {
+            super();
+            const template = $("person-display-template").content;
+            const shadowRoot = this.attachShadow({mode: 'open'});
+            let newNode = template.cloneNode(true);
+            shadowRoot.appendChild(newNode);
+        }
+    }
+);
+
+/* Unloads preloaded elements like thumbnails, when everything was loaded */
+document.addEventListener("DOMContentLoaded", () => {
+    let lazyBackgroundElement = document.querySelector(".lazy-background");
+    if ("IntersectionObserver" in window) {
+        let lazyObserver = new IntersectionObserver( (entries, observer) => {
+            entries.forEach( (entry) => {
+                if (entry.isIntersecting) {
+                    unloadPreloadedElements();
+                }
+            });
+        });
+        lazyObserver.observe(lazyBackgroundElement);
+    } else log("no intersection observer");
+});
+
+/* Creates and renders a person tree on body load */
+function onPageLoaded() {
+    // unloadPreloadedElements();
+    if (initialNbPersonsInput != undefined) {
+        const input = $('nbPersonsInput');
+        input.value=initialNbPersonsInput;
+        input.dispatchEvent(new KeyboardEvent('keydown', {keyCode:13}));
+    }
+}
+
+/* Removes the DOM elements to unload */
+function unloadPreloadedElements() {
+    $('loading-image').remove();
+    let lazyBackgroundElement = document.querySelector(".lazy-background");
+    lazyBackgroundElement.classList.remove("lazy-background");
+    lazyBackgroundElement.classList.add("background");
+}
+
+/* Develops and displays all the childs element on click */
+function developChildren(personTreeNode, mustDisplay) {
+    const personContainer = $("person"+personTreeNode.jsonIndex);
+    if (mustDisplay) {
+        const newList = document.createElement("ul");
+        personContainer.appendChild(newList);
+        recDevelopChild(personTreeNode, [...personTreeNode.childNodes]);
+    } else {
+        const ul = document.querySelector(`#person${personTreeNode.jsonIndex} ul:first-of-type`);
+        if (ul) {
+            for (li of document.querySelector(`#person${+personTreeNode.jsonIndex}`).querySelectorAll("ul li")) {
+                li.querySelector("div person-display").shadowRoot.querySelector("#mainDiv")
+                    .classList.add("close");
+            }
+            const timeOut = parseFloat(
+                window.getComputedStyle($(`person0`).querySelector("person-display")
+                .shadowRoot.querySelector("#mainDiv")).getPropertyValue("--animation-time")
+            ) * 1000;
+            setTimeout(() => {ul.remove()}, timeOut);
+        }
+    }
+}
+
+/* Recursively develops and displays each individual person */
+function recDevelopChild(personTreeNode, children) {
+    if (children.length==0)
+        return;
+    const personContainer = document.querySelector(`#person${personTreeNode.jsonIndex}`);
+    const childNode = children.shift();
+    const childElement = applyPersonTemplate(childNode);
+    const listElement = document.createElement("li");
+    listElement.appendChild(childElement);
+    const unorderedList = personContainer.querySelector(`ul:first-of-type`);
+    unorderedList.appendChild(listElement);
+    recDevelopChild(personTreeNode, children);
+}
+
+/* Applys the shadow template for one person's display */
+function applyPersonTemplate(personTreeNode) {
+    const nodeData = personsData[personTreeNode.jsonIndex];
+    let personContainer = document.createElement("div");
+    personContainer.id = "person"+personTreeNode.jsonIndex;
+    personContainer.isOpened = "false";
+    let personDisplay = document.createElement("person-display");
+    personDisplay.innerHTML = `
+        <a slot="hasChildren"></a>
+        <img slot="portrait" src="${nodeData.picture.large}" class="img" alt="portrait not found" />
+        <span slot="person-name">${nodeData.name.first} ${nodeData.name.last}</span>`;
+    let caretDiv = personDisplay.shadowRoot.querySelector("#mainDiv .caret-standard");
+    if ( personTreeNode.childNodes.length==0 )
+        caretDiv.remove()
+    let recNode = personTreeNode;
+    let personMainDiv = personDisplay.shadowRoot.querySelector("#mainDiv");
+    let backgroundColor = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--default-person-background-color"));
+    const i = 0x000D00 - 0x0D0000;
+    while (recNode.parentNode!=null && backgroundColor+i<0xFFFFFF) {
+        backgroundColor += i;
+        recNode = recNode.parentNode;
+    };
+    personMainDiv.setAttribute("style",`background-color:#${backgroundColor.toString(16)}`);
+    personMainDiv.onclick = (event) => {
+        const isOpened = JSON.parse(personContainer.isOpened);
+        personContainer.isOpened = JSON.stringify(! isOpened);
+        developChildren(personTreeNode, ! isOpened );
+    }
+    personContainer.appendChild(personDisplay);
+    return personContainer;
+}
+
+/*     #endregion     */
