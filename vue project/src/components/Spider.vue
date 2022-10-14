db935d8871c1cce31ce68df2533a00e15333cac9
David Baschung <davidbaschung1@gmail.com>
2022-10-14 18:27:51 +0200

    animations work, spider resizing

--- vue project/src/components/Spider.vue
+++ vue project/src/components/Spider.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="spider-selector" @click="openWidget">
+        <div class="spider-selector" @click="openWidget" ref="spider">
             <div class="hexagon"></div>
         </div>
         <div class="overlay" hidden></div>
@@ -32,20 +32,35 @@
                 context.appendChild(newHexagon);
                 this.addHexagon(newHexagon, --countDown);
             },
-            updateHexagonClass(context, className, isRemove=false) {
-                isRemove ? context.classList.remove(className) : context.classList.add(className);
+            updateHexagonClass(context, className, isAppending=true) {
+                isAppending ? context.classList.add(className) : context.classList.remove(className);
                 if (context.children.length != 0)
-                    this.updateHexagonClass(context.children[0], className, isRemove);
+                    this.updateHexagonClass(context.children[0], className, isAppending);
+                void context.offsetWidth; /* referencing a position attribute without effect allow the CSS animation to restart on classList update */
             },
             openWidget() {
                 let spider = document.getElementsByClassName("spider-selector")[0];
+                this.updateHexagonClass(spider, "close", false);
                 this.updateHexagonClass(spider, "open");
-                document.getElementsByClassName("overlay")[0].removeAttribute("hidden");
+                let overlay = document.getElementsByClassName("overlay")[0];
+                overlay.classList.add("open");
+                overlay.removeAttribute("hidden");
             },
             closeWidget() {
                 let spider = document.getElementsByClassName("spider-selector")[0];
-                this.updateHexagonClass(spider, "open", true);
-                document.getElementsByClassName("overlay")[0].setAttribute("hidden", true);
+                this.updateHexagonClass(spider, "open", false);
+                this.updateHexagonClass(spider, "closing");
+                void spider.offsetWidth;
+                setTimeout(() => this.updateHexagonClass(spider, "closing", false), 500);
+                let overlay = document.getElementsByClassName("overlay")[0];
+                overlay.classList.remove("open");
+                overlay.classList.add("closing");
+                void overlay.offsetWidth;
+                setTimeout(() => {
+                    overlay.classList.remove("closing");
+                    overlay.setAttribute("hidden", true);
+                    void overlay.offsetWidth;
+                }, 500);
             }
         },
         beforeCreate() {
@@ -56,13 +71,13 @@
             root.style.setProperty("--hexagon-right", ( widthGap + hexagonWidth ) * 100 + '%')
         },
         mounted() {
+            let spider = document.getElementsByClassName("spider-selector")[0];
             let root = document.documentElement;
             root.style.setProperty("--basis-color", this.primaryColor);
             let hexagonZero = document.getElementsByClassName("hexagon")[0];
-            this.addHexagon(hexagonZero, 3);
-            let spider = document.getElementsByClassName("spider-selector")[0];
+            this.addHexagon(hexagonZero, 5);
             window.addEventListener("click", (event) => {
-                if ( ! spider.contains(event.target))
+                if ( ! spider.contains(event.target) && spider.classList.contains("open"))
                     this.closeWidget();
             });
             window.addEventListener("keydown", (event) => {
@@ -74,39 +89,46 @@
 </script>
 
 <style>
-    @keyfranes open {
+    @keyframes open__spider {
         from {
-            background-color:yellow !important;
-            width: 40px;
+            transform: scale(20%);
+            top: -89px;
+            border-radius: 65px;
         }
         to {
-            background-color:purple !important;
-            width: 250px;
+            transform: scale(100%);
+            top: 20px;
+            border-radius: 30px;
         }
     }
     .spider-selector {
-        width: 40px;
-        height: 40px;
+        width: 30px;
+        height: 30px;
         padding: 4px;
         background-color: dodgerblue !important;
-        border-radius: 100em;
+        box-shadow: 0px 0px 3px 0px #0ff;
+        border-radius: 30px;
     }
-    .spider-selector.open {
+    .spider-selector.open, .spider-selector.closing {
         position: absolute;
-        left: 38em;
-        width: 200px;
-        height: 200px;
-        padding: 0;
+        left: 35.3em;
+        width: 240px;
+        height: 240px;
+        padding: 10px;
         border: 10px solid royalblue;
-        /* background-color: red !important; */
-        animation-name: open !important;
-        animation-duration: 2s;
+        box-shadow: none;
         z-index:3;
     }
+    .spider-selector.open {
+        animation: open__spider 0.5s normal;
+    }
+    .spider-selector.closing {
+        animation: open__spider 0.5s reverse;
+    }
     .hexagon {
-        padding: 4px;
-        width: calc(100% - 8px);
-        height: calc(100% - 8px);
+        padding: 2.5px;
+        width: calc(100% - 2 * 2.5px);
+        height: calc(100% - 2 * 2.5px);
         clip-path: polygon(
             var(--hexagon-left) 25%,
             var(--hexagon-left) 75%,
@@ -117,20 +139,39 @@
         );
         background-color: var(--basis-color);
     }
-    .hexagon.open {
+    .hexagon.open, .hexagon.closing {
         padding: 20px;
         width: calc(100% - 40px);
         height: calc(100% - 40px);
         z-index:3;
     }
+    @keyframes open__overlay {
+        from { opacity: 0% }
+        to { opacity: 50% }
+    }
+    @keyframes open__overlay2 {
+        to { opacity: 0% }
+        from { opacity: 50% }
+    }
     .overlay {
         position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
+        top: 10px;
+        left: 10px;
+        width: calc(100% - 20px);
+        height: calc(100% - 20px);
         background-color: darkgreen;
-        opacity: 50%;
+        box-shadow: 0px 0px 10px 10px #0ff;
+        opacity: 0%;
         z-index: 2;
+    }
+    .overlay.open, .overlay.closing {
+        opacity: 50%;
+    }
+    .overlay.open {
+        animation: open__overlay 0.5s normal;
+    }
+    .overlay.closing {
+        animation: open__overlay2 0.5s;
+        /* animation: open__overlay 0.5s reverse; */
     }
 </style>
